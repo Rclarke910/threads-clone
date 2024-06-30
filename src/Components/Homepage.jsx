fdc5c926ad016f9d961503891ed9a8aca53aaeab
Rashawn <Rashawnclarke1998@gmail.com>
2024-06-30 14:02:37 -0400

    test

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,11 +1,23 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const Homepage = () => {
   return (
   <div>
-    <h1>Homepage</h1>
-  </div>
+    <Grid className="px-5 lg:px-36 justify-betweeen">
+        <Grid item xs={0} lg={2.5} className="hidden lg:block w-full relative">
+            <p className ="text-center">middle part</p>
+        </Grid>
+        <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+            <p className='text-center'>middle part</p>
+        </Grid>
+
+        <Grid item xs={12} lg={6} className ='hidden lg:block w-full relative'>
+        <p className='text-center'>middle part</p>
+        </Grid>
+        
+    </Grid>
+    </div>
   )
 }
-
 export default Homepage
